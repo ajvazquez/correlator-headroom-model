fbd523ae2780873431aeb691ff6f311b8403175b
AJ Vazquez Alvarez <ajvazquez.teleco@gmail.com>
2022-11-17 19:33:11 +0100

    Feature: added general dashboard

--- correlator_headroom_model/representation.py
+++ correlator_headroom_model/representation.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+import ipywidgets as widgets
+
 plt.style.use('seaborn-whitegrid')
 
 
@@ -18,7 +20,7 @@ class ReprMixin:
     def show_blocks(self):
         print(self.blocks())
 
-    def show(self, ymin=None, ymax=None):
+    def show(self, ymin=None, ymax=None, legend_loc=None, show_text=False):
         values = self.cached_values()
 
         fig, ax = plt.subplots()
@@ -35,9 +37,43 @@ class ReprMixin:
         plt.xticks(x, xticks)
         plt.ylabel("R{}".format(" [{}]".format(self.units) if self.units else ""))
         plt.xlabel("Correlator stage")
+        if show_text:
+            plt.text(0, 0.9*self.R, "{} {}".format(eval("%.0e" % self.R), self.units), verticalalignment="top", color="blue")
+            plt.text(len(self.values)-3, 0.9*self.values[-1], "{} {}".format(eval("%.0e" % self.values[-1]), self.units), verticalalignment="top", color="lightgrey")
+        if ymin is None:
+            ymin = min(self.values)/10
         if ymin:
             plt.ylim(bottom=ymin)
         if ymax:
             plt.ylim(top=ymax)
-        plt.legend(loc="lower left")
+        if legend_loc is None:
+            legend_loc = "best"
+        plt.legend(loc=legend_loc)
 
+
+def dashboard(model, parameters):
+    """
+    Simple dashboard for a model with parameters (param, default, step, min, max, type).
+    """
+
+    dict_widgets = {
+        int: widgets.IntSlider,
+        float: widgets.FloatSlider
+    }
+
+    def show(*args, **kwargs):
+        m = model(*args, **kwargs)
+        m.show(show_text=True)
+
+    ws = {x[0]: dict_widgets[x[-1]](
+        value=x[1],
+        step=x[2],
+        min=x[3],
+        max=x[4],
+        description=x[0],
+        orientation="vertical",
+        layout=widgets.Layout(width="40px")) for x in parameters}
+    wi = widgets.interactive(show, **ws)
+    disp = widgets.HBox(wi.children)
+    wi.update()
+    display(disp)
--- setup.py
+++ setup.py
@@ -5,15 +5,19 @@ from distutils import sysconfig
 site_packages_path = sysconfig.get_python_lib()
 
 VERSION = "version.txt"
-REQUIREMENTS = "requirements.txt"
+REQUIREMENTS_V = [
+    "requirements.txt",
+    "requirements.dashboard.txt"
+]
 
 
 def get_path(fi):
     return os.path.join(os.path.dirname(__file__), fi)
 
-
-with open(get_path(REQUIREMENTS)) as f:
-    reqs = [line.strip() for line in f if "==" in line and not line.strip().startswith("#")]
+reqs = []
+for req in REQUIREMENTS_V:
+    with open(get_path(req)) as f:
+        reqs.extend([line.strip() for line in f if "==" in line and not line.strip().startswith("#")])
 
 with open(get_path(VERSION)) as f:
     version = [x.strip() for x in f][0]
